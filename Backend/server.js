78d4410c0920624088b718dbf7dd59a4e06c538d
NikhilJhankal21 <nikhiljhankal@gmail.com>
2026-07-22 23:25:37 +0530

    Setup MongoDB models and database connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,13 @@
-import app from './src/app.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import app from './src/app.js';
+import { connectDB } from './src/lib/db.js';
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
+connectDB();
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import { clerkMiddleware } from '@clerk/express'
+import cors from 'cors';
 
 const app=express();
+
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+
+app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(clerkMiddleware()) 
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'Server is healthy'  });
+});
 
 export default app;
