d925fc88dd17fbd53a719fd62dc6f1645c521665
NikhilJhankal21 <nikhiljhankal@gmail.com>
2026-07-23 20:25:10 +0530

    Add cron job to send health check requests every 14 minutes

--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,14 @@ dotenv.config();
 
 import app from './src/app.js';
 import { connectDB } from './src/lib/db.js';
+import job from './src/lib/cron.js';
 
 const PORT = process.env.PORT || 3000;
 
 connectDB();
+
+if(process.env.NODE_ENV !== 'production') job.start();
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
