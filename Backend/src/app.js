c9ea11649c63085585ff51ac3942e9be8ad38cea
NikhilJhankal21 <nikhiljhankal@gmail.com>
2026-07-23 16:11:12 +0530

    Add Dockerfile and .dockerignore; implement static file serving in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,9 +2,14 @@ import express from 'express';
 import { clerkMiddleware } from '@clerk/express'
 import cors from 'cors';
 
+import fs from 'fs';    
+import path from 'path';
 const app=express();
 
+
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+
+const pubDir = path.join(process.cwd(), 'public');
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
@@ -14,4 +19,15 @@ app.get('/health', (req, res) => {
     res.status(200).json({ message: 'Server is healthy'  });
 });
 
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
+
 export default app;
+
